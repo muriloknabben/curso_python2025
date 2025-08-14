ecef0e4e29c279072010532a6983784d72c217f1
Murilo Ferreira Knabben <murilofknabben@gmail.com>
2025-08-14 18:29:50 -0300

    aulas 12 a 16

--- aula12.py
+++ aula12.py
@@ -4,4 +4,6 @@ peso = 90
 
 imc = peso / altura ** 2
 
-print(f'O peso de {nome} é: {peso} \nSua altura é: {altura:.2f} \nE seu IMC é: {imc}') # :.2f serve para deixar com duas casas decimais
+print(f'O peso de {nome} é: {peso} \nSua altura é: {altura:.2f} \nE seu IMC é: {imc}')
+# :.xf serve para deixar com um numero x de casas decimais
+# também pode ser usado :,.2f para o numero sair disso -> 100000 para isso -> 100,000.00

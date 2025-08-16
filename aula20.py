496ed0d38478fa7e1fba7e30ecc6bc6b64c5aab5
Murilo Ferreira Knabben <murilofknabben@gmail.com>
2025-08-16 02:28:17 -0300

    aulas 17 a 21

---
+++ aula20.py
@@ -0,0 +1,11 @@
+primeiro_valor = input('Digite o primeiro valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+
+if primeiro_valor > segundo_valor:
+    print('O primeiro valor é maior que o segundo.')
+
+elif segundo_valor > primeiro_valor:
+    print('O segundo valor é maior que o primeiro.')
+
+else:
+    print('Os valores são iguais.')

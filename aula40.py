04efa26311d25670bcbdf10f5927069e58d21e9b
Murilo Ferreira Knabben <murilofknabben@gmail.com>
2025-08-22 23:43:43 -0300

    resolução aula40

--- aula40.py
+++ aula40.py
@@ -1,54 +1,34 @@
 while True:
     operacao = input('Digite "a" para contas de adição\nDigite "m" para contas de menos\nDigite "v" para contas de vezes\nDigite "d" para contas de divisão\nDigite "s" para sair\nO que deseja: ')
     operacao = operacao.lower()
-
-    if operacao == 'a':
+    if operacao in ('a', 'm', 'v', 'd'):
         try:
             a = input('Digite o primeiro numero: ')
             b = input('Digite o segundo número: ')
             a = float(a)
             b = float(b)
-            c = a + b
-            print(f'{a} + {b} = {c}')
         except ValueError:
             print('Por favor digite um número')
+
+    elif operacao == 'a':
+        c = a + b
+        print(f'{a} + {b} = {c}')
     
     elif operacao == 'm':
-        try:
-            a = input('Digite o primeiro numero: ')
-            b = input('Digite o segundo número: ')
-            a = float(a)
-            b = float(b)
             c = a - b
             print(f'{a} - {b} = {c}')
-        except ValueError:
-            print('Por favor digite um número')
 
     elif operacao == 'v':
-        try:
-            a = input('Digite o primeiro numero: ')
-            b = input('Digite o segundo número: ')
-            a = float(a)
-            b = float(b)
             c = a * b
             print(f'{a} x {b} = {c}')
-        except ValueError:
-            print('Por favor digite um número')
 
     elif operacao == 'd':
-        try:
-            a = input('Digite o primeiro numero: ')
-            b = input('Digite o segundo número: ')
-            a = float(a)
-            b = float(b)
-            if b == 0:
-                print('Divisão por 0 não é possivel, por favor digite uma operção válida')
+        if b == 0:
+            print('Divisão por 0 não é possivel, por favor digite uma operção válida')
             
-            else:
-                c = a / b
-                print(f'{a} / {b} = {c}')
-        except ValueError:
-            print('Por favor digite um número')
+        else:
+            c = a / b
+            print(f'{a} / {b} = {c}')
 
     elif operacao == 's':
         break

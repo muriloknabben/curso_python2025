52629cb9f4766c0d77c7dba92c656fa574354f22
Murilo Ferreira Knabben <murilofknabben@gmail.com>
2025-08-22 22:33:30 -0300

    aula39 dnv

--- aula39.py
+++ aula39.py
@@ -2,11 +2,30 @@
 Iterando strings com while
 """
 
-nome = 'Murilo Knabben'  # Iteráveis
-tamanho_nome = len(nome)
+nome1 = 'Murilo Knabben'  # Iteráveis
+tamanho_nome = len(nome1)
 
+nome_res = ''
 i = 0
 while i < tamanho_nome:
-    letra = nome[i]
-    print(f"Caractere {i}: {letra}")
+    letra1 = nome1[i]
+    nome_res += f'*{letra1}'
     i += 1
+
+print(nome_res)
+
+
+#resolução do professor:
+
+
+nome2 = 'Maria Helena'  # Iteráveis
+
+indice = 0
+novo_nome = ''
+while indice < len(nome2):
+    letra2 = nome2[indice]
+    novo_nome += f'*{letra2}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)

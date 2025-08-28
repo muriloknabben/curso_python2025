7daed3be190daec705af3b9f99c1c7af65858e94
Murilo Ferreira Knabben <murilofknabben@gmail.com>
2025-08-28 20:35:40 -0300

    teachers soluction on exercise 75

--- aula75.py
+++ aula75.py
@@ -11,3 +11,19 @@ number = multiplicate(4)
 
 for n in [2, 3, 4]:
     print(number(n))
+
+# Solução professor:
+
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+
+duplicar = criar_multiplicador(2)
+triplicar = criar_multiplicador(3)
+quadruplicar = criar_multiplicador(4)
+
+print(duplicar(2))
+print(triplicar(2))
+print(quadruplicar(2))

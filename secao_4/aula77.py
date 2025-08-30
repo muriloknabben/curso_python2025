51620d2bc1c1bfabb5e395b8da2689facbac9143
Murilo Ferreira Knabben <murilofknabben@gmail.com>
2025-08-29 22:26:06 -0300

    organização + aula76 a 82

---
+++ secao_4/aula77.py
@@ -0,0 +1,74 @@
+import os
+
+perguntas = [
+    {
+        'Pergunta': 'Quanto é 2+2?',
+        'Opções': ['1', '3', '4', '5'],
+        'Resposta': '4',
+    },
+    {
+        'Pergunta': 'Quanto é 5*5?',
+        'Opções': ['25', '55', '10', '51'],
+        'Resposta': '25',
+    },
+    {
+        'Pergunta': 'Quanto é 10/2?',
+        'Opções': ['4', '5', '2', '1'],
+        'Resposta': '5',
+    },
+]
+
+item = 0
+while item in range(len(perguntas)):
+    pergunta = perguntas[item]
+    resposta = input(f'{pergunta['Pergunta']}\nOpções = {pergunta['Opções']}\n')
+    if resposta not in pergunta['Opções']:
+        print('A resposta é uma das opções.')
+    else:
+        if resposta == pergunta['Resposta']:
+            print('Parabéns você acertou, responda a proxima pergunta')
+            item += 1
+        else:
+            print('Errado, tente de novo.')
+
+os.system('cls')
+print('Você acertou todas as perguntas!')
+
+# correção professor:
+
+qtd_acertos = 0
+for pergunta in perguntas:
+    print('Pergunta:', pergunta['Pergunta'])
+    print()
+
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f'{i})', opcao)
+    print()
+
+    escolha = input('Escolha uma opção: ')
+
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+
+    print()
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou 👍')
+    else:
+        print('Errou ❌')
+
+    print()
+
+
+print('Você acertou', qtd_acertos)
+print('de', len(perguntas), 'perguntas.')
